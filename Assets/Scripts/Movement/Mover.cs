98552a9bff633675267a11a6f7aebd8158d66b60
Brian Akbar <brianakbar@yahoo.com.sg>
2023-04-18 19:33:29 +0700

    Delete animation module

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -3,17 +3,23 @@ namespace Creazen.Wizard.Movement {
     using UnityEngine;
     
     public class Mover : MonoBehaviour {
+        [SerializeField] Animator body;
         [SerializeField] ActionScheduler movementScheduler;
         [SerializeField] Idle idle;
         [SerializeField] Move move;
 
         MoveLink moveActionLink;
         IdleLink idleActionLink;
+        Rigidbody2D rb2D;
 
         void Awake() {
-            movementScheduler = GetComponent<ActionScheduler>();
+            rb2D = GetComponent<Rigidbody2D>();
             moveActionLink = Move.GetLink();
             idleActionLink = Idle.GetLink();
+        }
+
+        void Update() {
+            body.SetBool("hasSpeed", rb2D.velocity != Vector2.zero);
         }
 
         public void StartMoving(Vector2 direction) {
